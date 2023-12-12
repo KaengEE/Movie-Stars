b59ebb51513ddaa6422cce90a14525fa67fe66e9
KaengEE <yopuiqwer@naver.com>
2023-12-12 11:45:21 +0900

    회원가입(자체 계정)

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./Join.css";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth } from "../firebase";
 
 export default function Join() {
   const [isLoading, setLoading] = useState(false);
@@ -10,11 +12,62 @@ export default function Join() {
   const [error, setError] = useState("");
   const navigate = useNavigate(); //네브객체 - 페이지 이동
 
+  // 입력시
+  const onChange = (e) => {
+    //console.log(e);
+    const { name, value } = e.target;
+    if (name === "name") {
+      setName(value);
+    } else if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  //submit함수
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setError(null); //에러초기화
+
+    // 공백일 경우 return
+    if (
+      isLoading ||
+      name.trim() === "" ||
+      email.trim() === "" ||
+      password.trim() === ""
+    )
+      return;
+    try {
+      //console.log(name, email, password);
+      setLoading(true);
+      const credentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      //이름 업데이트
+      await updateProfile(credentials.user, {
+        displayName: name,
+      });
+      // home으로 이동
+      navigate("/");
+    } catch (e) {
+      // 에러발생시
+      if (e instanceof FirebaseError) {
+        //console.log(e.code);
+        setError(errorMessageToKorean(e));
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div className="join-container">
       <h1>Join Movie Stars</h1>
-      <form className="join-form">
+      <form className="join-form" onSubmit={onSubmit}>
         <input
+          onChange={onChange}
           type="text"
           name="name"
           value={name}
@@ -22,6 +75,7 @@ export default function Join() {
           required
         />
         <input
+          onChange={onChange}
           type="email"
           name="email"
           value={email}
@@ -29,6 +83,7 @@ export default function Join() {
           required
         />
         <input
+          onChange={onChange}
           type="password"
           name="password"
           value={password}
