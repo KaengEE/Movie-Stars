bd930092de056c3e956776845a9eb2c23fa71c7a
KaengEE <yopuiqwer@naver.com>
2023-12-12 11:45:04 +0900

    firebase 설정 수정

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,8 @@
 //firebase 설정
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 // 개인설정
 const firebaseConfig = {
@@ -11,16 +14,16 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_APP_ID,
 };
 
-//본인 계정의 설정값으로 초기화
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 // firestore의 DB서비스
-const db = firebase.firestore();
+const db = getFirestore(app);
 
 //인증서비스
-const auth = firebase.auth();
+const auth = getAuth(app);
 
-//시간
-const timestamp = firebase.firestore.Timestamp;
+// 스토리지
+const storage = getStorage(app);
 
-export { db, auth, timestamp };
+export { db, auth, storage };
