d89a5d7cde9b75ba9b699f7d36c276551e5c143f
KaengEE <yopuiqwer@naver.com>
2023-12-13 12:51:50 +0900

    마이페이지 본인이 작성한 평가 가져오기

---
+++ src/components/Comment/MyComment.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { collection, query, where, orderBy, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
+import "./Comment.css";
+import OneComment from "./OneComment";
+
+export default function MyComment({ userId }) {
+  const [comments, setComments] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      try {
+        const q = query(
+          collection(db, "comments"),
+          where("userId", "==", userId),
+          orderBy("createdAt", "desc")
+        );
+        const snapshot = await getDocs(q);
+        const commentsData = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setComments(commentsData);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching comments:", error);
+      }
+    };
+    fetchComments();
+  }, [userId]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (comments.length === 0) {
+    return <p>평가가 없습니다.</p>;
+  }
+  return (
+    <>
+      {comments.map((comment) => (
+        <OneComment key={comment.id} {...comment} />
+      ))}
+    </>
+  );
+}
--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,21 +1,63 @@
-import React from "react";
-import { auth } from "../firebase";
+import React, { useEffect, useState } from "react";
 import "./Mypage.css";
+import { auth, db } from "../firebase";
+import OneComment from "../components/Comment/OneComment";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
 
-//유저 프로필
+// 유저 프로필
 export default function Mypage() {
-  //현재유저
   const user = auth.currentUser;
+  const [comments, setComments] = useState([]);
+
+  //유저가 작성한 평가
+  const fetchComments = async () => {
+    const q = query(
+      collection(db, "comment"),
+      where("userId", "==", user?.uid),
+      orderBy("createdAt", "desc")
+    );
+    const snapshot = await getDocs(q);
+    const comments = snapshot.docs.map((doc) => {
+      const {
+        comment,
+        createdAt,
+        userId,
+        username,
+        userProfile,
+        stars,
+        movieId,
+      } = doc.data();
+      return {
+        comment,
+        createdAt: new Date(createdAt).toLocaleDateString("ko-KO"),
+        userId,
+        username,
+        userProfile,
+        stars,
+        movieId,
+        id: doc.id,
+      };
+    });
+    setComments(comments);
+  };
+
+  //시작할때 가져오기
+  useEffect(() => {
+    fetchComments();
+  }, []);
 
   return (
     <div className="mypage-container">
       <div className="profile">
-        <img src={user.photoURL} />
-        <p>{user.displayName}</p>
-        <button>프로필수정</button>
+        {user?.photoURL && <img src={user?.photoURL} alt="User Avatar" />}
+        {user?.displayName && <p>{user?.displayName}</p>}
+        <button>프로필 수정</button>
       </div>
       <div className="my-comments">
         <p>내 평가</p>
+        {comments.map((comment) => (
+          <OneComment key={comment.id} {...comment} />
+        ))}
       </div>
     </div>
   );
