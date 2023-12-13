18857cb50c32f04a41f3a752cac8cf685424d7bb
KaengEE <yopuiqwer@naver.com>
2023-12-13 14:46:24 +0900

    app에 firebase 인증추가

--- src/App.jsx
+++ src/App.jsx
@@ -8,35 +8,56 @@ import Login from "./pages/Login";
 import Join from "./pages/Join";
 import Search from "./components/Search/Search";
 import Mypage from "./pages/Mypage";
+import { useEffect, useState } from "react";
+import { auth } from "./firebase";
+import LoadingSpinner from "./pages/LoadingSpinner";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  //초기화 함수
+  const init = async () => {
+    //파이어베이스 체크
+    await auth.authStateReady();
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
   return (
     <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/popular"
-          element={<MovieList type="popular" title="인기작품" />}
-        />
-        <Route
-          path="/top_rated"
-          element={<MovieList type="top_rated" title="최고평점" />}
-        />
-        <Route
-          path="/upcoming"
-          element={<MovieList type="upcoming" title="개봉예정" />}
-        />
-        <Route path="/search" element={<Search />} />
-        {/* 로그인/회원가입 */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/join" element={<Join />} />
-        {/* 마이페이지 */}
-        <Route path="/mypage" element={<Mypage />} />
-
-        {/* 상세페이지 */}
-        <Route path="/movie/:movieId" element={<SingleMovie />} />
-      </Routes>
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/popular"
+              element={<MovieList type="popular" title="인기작품" />}
+            />
+            <Route
+              path="/top_rated"
+              element={<MovieList type="top_rated" title="최고평점" />}
+            />
+            <Route
+              path="/upcoming"
+              element={<MovieList type="upcoming" title="개봉예정" />}
+            />
+            <Route path="/search" element={<Search />} />
+            {/* 로그인/회원가입 */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/join" element={<Join />} />
+            {/* 마이페이지 */}
+            <Route path="/mypage" element={<Mypage />} />
+            {/* 상세페이지 */}
+            <Route path="/movie/:movieId" element={<SingleMovie />} />
+          </Routes>
+        </>
+      )}
     </div>
   );
 }
